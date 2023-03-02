ca65801878914c45fa063628d4c32269a99ad43d
Michelle <placenciamitel14@gmail.com>
2023-03-02 22:47:48 +0800

    February 18, 2023 - Linear, Relative, and Constrain Layout

--- app/src/main/java/com/example/testapplication/SecondActivity.java
+++ app/src/main/java/com/example/testapplication/SecondActivity.java
@@ -14,15 +14,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button = (Button) findViewById(R.id.send2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
-    Button button = (Button) findViewById(R.id.send2);
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
-            startActivity(intent);
-        }
-    });
-
-
 }
